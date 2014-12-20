dc65929f245ee35c18c1c37ba926cc5a6830f157
David Fedele <dave.fedele@gmail.com>
2014-12-19 16:20:41 -0800

    added player as rect

--- watchout.js
+++ watchout.js
@@ -10,22 +10,45 @@ var svg = d3.select("body")
 
 var dataset = [ 5, 10, 15, 20, 25, 30];
 
-var circles = svg.selectAll("circle")
-                .data(dataset).enter()
-                .append("circle");
+var initializePlayer = function(x,y){
+  x = x || (0.5 * w);
+  y = y || (0.5 * h);
+  svg.selectAll("rect")
+    .data([{x:x, y:y}])
+    .enter()
+    .append("rect")
+    .attr("x", function(d) {
+              return d.x;})
+    .attr("y",  function(d) {
+              return d.y;})
+    .attr("height", function(d){
+      return 20;
+    })
+    .attr("width", function(d) {
+      return 20;
+    })
+    .attr("class", "player")
+    .style("fill", "steelblue");
+}
 
-circles.attr("cx", function(d, i) {
-    return (i * 50) + 25;
-  })
-  .attr("cy", h/2)
-  .attr("r", function(d) {
-    return d;
-});
+var createCircles = function(dataset) {
+  return svg.selectAll("circle")
+            .data(dataset).enter()
+            .append("circle")
+            .attr("cx", function(d, i) {
+              return (i * 50) + 25;
+            })
+            .attr("cy", h/2)
+            .attr("r", function(d) {
+              return d;
+          });
+};
 
 var circlesTransition = function(){
+  var circles = svg.selectAll("circle")[0].length;
 
   var result = [];
-  for( var i=0; i<circles[0].length; i++ ) {
+  for( var i=0; i<circles; i++ ) {
     result.push(Math.random() * 35 + 10);
   }
 
@@ -45,6 +68,10 @@ var circlesTransition = function(){
       })
       .style("fill-opacity", 1);
 };
+
+initializePlayer();
+createCircles(dataset);
+
 //declare as var so we can stop setInterval call
 var running = setInterval(circlesTransition, 4000);
 
